0c986d8e8cbe364de0b91cfac6133054e46ddb31
admirestator <admirestator@gmail.com>
2012-05-10 09:11:13 +0800

    add arguments of bg and fg color

--- config.h
+++ config.h
@@ -19,6 +19,12 @@ public:
     //default settings
     int default_alertTime;
     short default_restTime;
+    unsigned short default_bg_colorR;
+    unsigned short default_bg_colorG;
+    unsigned short default_bg_colorB;
+    unsigned short default_fg_colorR;
+    unsigned short default_fg_colorG;
+    unsigned short default_fg_colorB;
     bool default_start_with_system;
     bool default_playSound;
     bool default_rest_with_fullscreen;
@@ -27,14 +33,18 @@ public:
 
     int alertTime;
     short restTime;
+    unsigned short bg_colorR;
+    unsigned short bg_colorG;
+    unsigned short bg_colorB;
+    unsigned short fg_colorR;
+    unsigned short fg_colorG;
+    unsigned short fg_colorB;
     bool start_with_system;
     bool playSound;
     bool rest_with_fullscreen;
     QString language;
 
-
     QString version;
-
 
 public slots:
 
